497ba94b62b1774e774dacb96fd14897e085a023
MikeLearnsIT <eagleyuanhang@163.com>
2025-12-19 10:09:47 +0800

    修复Vue Router 的配置

--- src/router/index.js
+++ src/router/index.js
@@ -120,7 +120,7 @@ const my_routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: my_routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
